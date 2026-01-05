97272516e5d181c760c95b34b110c4e1af848e59
nickcube2 <awuninicholas@gmail.com>
2026-01-05 15:13:30 -0700

    changes to variables.tf

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,16 +1,64 @@
 variable "aws_region" {
-  default = "us-east-1"
+  description = "AWS region to deploy resources into"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "cluster_name" {
-  default = "cobank-eks"
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "cobank-eks"
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version for the EKS cluster"
+  type        = string
+  default     = "1.31"
 }
 
 variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "github_repo_url" {
-  description = "Your GitHub repo URL for ArgoCD (e.g., https://github.com/yourusername/cobank-cloud-platform.git)"
+  description = "Full HTTPS URL of your GitHub repository"
   type        = string
+}
+
+variable "github_branch" {
+  description = "Branch for ArgoCD to sync"
+  type        = string
+  default     = "main"
+}
+
+variable "node_instance_type" {
+  type    = string
+  default = "t3.medium"
+}
+
+variable "node_min_size" {
+  type    = number
+  default = 2
+}
+
+variable "node_max_size" {
+  type    = number
+  default = 6
+}
+
+variable "node_desired_size" {
+  type    = number
+  default = 3
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default = {
+    Project     = "CoBank Cloud Platform"
+    Environment = "prod"
+    ManagedBy   = "Terraform"
+  }
 }
